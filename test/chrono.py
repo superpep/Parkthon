dab2a364d4c1c6d890e6cac4cd39abe1d22794f3
Àlex <alexcrafteo1@outlook.es>
2020-11-13 19:54:19 +0100

    Gorrinà de crono però funciona

--- test/chrono.py
+++ test/chrono.py
@@ -1,11 +1,47 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtCore
 import sys
+import datetime
+import time
 
 class Chrono(QtWidgets.QMainWindow):
     def __init__(self):
         super(Chrono, self).__init__() # Call the inherited classes __init__ method
         uic.loadUi('UI/cronometro.ui', self) # Load the .ui file
         self.show() # Show the GUI
+        self.startStop.clicked.connect(self.start_crono)
+        self.lap.clicked.connect(self.record_lap)
+
+
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self.run_watch)
+        self.timer.setInterval(1)
+        self.mscounter = 0
+        self.isreset = True
+
+    def record_lap(self):
+        print(self.cronNum.text())
+
+    def showLCD(self):
+        text = str(datetime.timedelta(milliseconds=self.mscounter))[:-3]
+        if not self.isreset:  # if "isreset" is False
+            self.cronNum.setText(text)
+        else:
+            self.cronNum.setText('0:00:00.000')
+
+    def run_watch(self):
+        self.mscounter += 1
+        self.showLCD()
+
+    def start_crono(self):
+        self.timer.start()
+        self.isreset = False
+        self.startStop.clicked.connect(self.stop_watch)
+    
+    def stop_watch(self):
+        self.timer.stop()
+        self.mscounter = 0
+        self.cronNum.setText('0:00:00.000')
+        self.startStop.clicked.connect(self.start_crono)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
