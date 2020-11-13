72de10b77611c21c4dd8b6c4f73ad5ea18259636
superpep <ladiv12@pm.me>
2020-11-13 17:50:41 +0100

    Solucionat problema que deixava la finestra penjada quan el login era correcte

--- test/login.py
+++ test/login.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtWidgets, uic
 import sqlite_connector as sqlite
 import sys
-
+import chrono
 
 DB = "DB/parkthon.db" # RUTA DE LA BASE DE DADES
 
@@ -16,9 +16,7 @@ class Ui(QtWidgets.QMainWindow):
         if(sqlite.database_exists(DB)):
             sql_con = sqlite.sqlite_connector(DB)
             if(sql_con.login(self.user.text(), self.passwd.text())):
-                self.errorLabel.hide()
-                import chrono
-                self.close()
+                self.open_new_window()
                 
             else:
                 self.errorLabel.setText("Error. DNI i/o contrasenya incorrecta")
@@ -28,9 +26,14 @@ class Ui(QtWidgets.QMainWindow):
             sql_con.create_initial_table()
             sql_con.create_user(self.user.text(), self.passwd.text())
             # TO-DO: Implementar aço, no sabem si fer una finestra nova on demanar dades.
+    
+    def open_new_window(self):
+        self.new_window = chrono.Chrono()
+        self.new_window.show()
+        self.close()
         
 
-
-app = QtWidgets.QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
-window = Ui() # Create an instance of our class
-app.exec_() # Start the application
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
+    window = Ui() # Create an instance of our class
+    app.exec_() # Start the application
