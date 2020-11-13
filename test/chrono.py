cc4df1b47944b44f4aa822cfe8638c132e2370d3
superpep <ladiv12@pm.me>
2020-11-13 17:50:59 +0100

    Introduïda funció que comprova el main per a evitar problemes

--- test/chrono.py
+++ test/chrono.py
@@ -1,11 +1,13 @@
 from PyQt5 import QtWidgets, uic
 import sys
 
-class Ui(QtWidgets.QMainWindow):
+class Chrono(QtWidgets.QMainWindow):
     def __init__(self):
-        super(Ui, self).__init__() # Call the inherited classes __init__ method
+        super(Chrono, self).__init__() # Call the inherited classes __init__ method
         uic.loadUi('UI/cronometro.ui', self) # Load the .ui file
         self.show() # Show the GUI
-app = QtWidgets.QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
-window = Ui() # Create an instance of our class
-app.exec_() # Start the application
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
+    window = Chrono() # Create an instance of our class
+    app.exec_() # Start the application
