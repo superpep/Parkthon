673313fbc0196e18fad9c35f49ca841478be563a
Àlex <alexcrafteo1@outlook.es>
2020-11-15 10:45:34 +0100

    Afegida funcionalitat a botons

--- test/chrono.py
+++ test/chrono.py
@@ -12,6 +12,7 @@ class Chrono(QtWidgets.QMainWindow):
         self.lap.clicked.connect(self.record_lap)
 
 
+        self.paused = False
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self.run_watch)
         self.timer.setInterval(1)
@@ -19,15 +20,17 @@ class Chrono(QtWidgets.QMainWindow):
         self.isreset = True
 
     def record_lap(self):
-        print(self.cronNum.text())
+        time = str(datetime.timedelta(milliseconds=self.mscounter))[:-3]
+        if (not time == "0:00"):
+            print(time)
 
     def showLCD(self):
         text = str(datetime.timedelta(milliseconds=self.mscounter))[:-3]
         self.cronNum.setDigitCount(8)
-        if not self.isreset:  # if "isreset" is False
+        if not self.isreset:  # si "isreset" es False
             self.cronNum.display(text)
         else:
-            self.cronNum.display('00:00.000')
+            self.cronNum.display('0:00.000')
 
     def run_watch(self):
         self.mscounter += 1
@@ -36,13 +39,30 @@ class Chrono(QtWidgets.QMainWindow):
     def start_crono(self):
         self.timer.start()
         self.isreset = False
-        self.startStop.clicked.connect(self.stop_watch)
+
+        self.lap.setText("Lap")
+        self.startStop.setText("Pause")
+        self.startStop.clicked.connect(self.pause_watch)
+        self.isreset = False
+        
+    def pause_watch(self):
+        self.timer.stop()
+
+        self.startStop.setText("Resume")
+        self.startStop.clicked.connect(self.start_crono)
+        self.lap.setText("Reset")
+        self.lap.clicked.connect(self.reset_watch)
     
-    def stop_watch(self):
+    def reset_watch(self):
         self.timer.stop()
         self.mscounter = 0
-        self.cronNum.display('00:00.000')
-        self.startStop.clicked.connect(self.start_crono)
+        self.isreset = True
+        self.showLCD()
+
+        self.startStop.setText("Start")
+        self.lap.setText("Lap")
+        self.lap.clicked.connect(self.record_lap)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
