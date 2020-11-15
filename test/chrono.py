2f8c1b0beb6e605f6b7681cacfb41e16800cfba9
Àlex <alexcrafteo1@outlook.es>
2020-11-15 21:16:39 +0100

    Canvis grans en chrono: - Ja no peta quan fas molt de clic en start i stop - Lap ja no reinicia la segona vegada - Les voltes es registren adecuadament

--- test/chrono.py
+++ test/chrono.py
@@ -1,4 +1,5 @@
-from PyQt5 import QtWidgets, uic, QtCore
+from PyQt5 import QtWidgets, uic, QtCore, QtGui
+#from PyQt5.QtWidgets import QLabel
 import sys
 import datetime
 import time
@@ -11,6 +12,13 @@ class Chrono(QtWidgets.QMainWindow):
         self.startStop.clicked.connect(self.start_crono)
         self.lap.clicked.connect(self.record_lap)
     
+
+        self.pauseImage = QtGui.QIcon('img/pause.png')
+        self.start_image = QtGui.QIcon('img/white.png')
+        self.pause_image = QtGui.QIcon('img/pause.png')
+
+        self.lap_num = 0
+        self.previous_time = 0
         self.paused = False
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self.run_watch)
@@ -20,9 +28,19 @@ class Chrono(QtWidgets.QMainWindow):
         self.showLCD() 
 
     def record_lap(self):
-        time = str(datetime.timedelta(milliseconds=self.mscounter))[:-3]
-        if (not time == "0:00"):
-            print(time)
+        
+        this_time = datetime.timedelta(milliseconds=self.mscounter)
+        self.lap_num += 1
+        text = "Lap "+str(self.lap_num)+": "
+        if (not this_time == "0:00"):
+            if(self.lap_num ==  1):
+                text += str(this_time)[2:-3]
+            else:
+                text += str(this_time - self.previous_time)[2:-3]
+            
+            self.previous_time = this_time
+            self.lapsLayout.addWidget(QtWidgets.QLabel(text))
+
 
     def showLCD(self):
         text = str(datetime.timedelta(milliseconds=self.mscounter))[:-3]
@@ -38,30 +56,44 @@ class Chrono(QtWidgets.QMainWindow):
 
     def start_crono(self):
         self.timer.start()
-        self.isreset = False
+        
 
         self.lap.setText("Lap")
-        self.startStop.setText("Pause")
+        if(self.isreset == False):
+            self.lap.clicked.disconnect(self.reset_watch)
+            self.lap.clicked.connect(self.record_lap)
+
+        self.startStop.setIcon(self.pause_image)
+        self.startStop.clicked.disconnect(self.start_crono)
         self.startStop.clicked.connect(self.pause_watch)
         self.isreset = False
         
     def pause_watch(self):
         self.timer.stop()
 
-        self.startStop.setText("Resume")
+        self.startStop.setIcon(self.start_image)
+        self.startStop.clicked.disconnect(self.pause_watch)
         self.startStop.clicked.connect(self.start_crono)
+
         self.lap.setText("Reset")
+        self.lap.clicked.disconnect(self.record_lap)
         self.lap.clicked.connect(self.reset_watch)
     
     def reset_watch(self):
         self.timer.stop()
         self.mscounter = 0
         self.isreset = True
+        self.previous_time = 0
         self.showLCD()
 
-        self.startStop.setText("Start")
+        self.startStop.setIcon(self.start_image)
+
+
+
         self.lap.setText("Lap")
+        self.lap.clicked.disconnect(self.reset_watch)
         self.lap.clicked.connect(self.record_lap)
+        
 
 # Eliminar aço despres de acabar les proves ja que no volem que es puga executar
 if __name__ == "__main__":
