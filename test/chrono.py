ca5028ee22d41e0028e0f8b428e10cf90f1d82a8
Àlex <alexcrafteo1@outlook.es>
2020-11-16 18:28:19 +0100

    Afegit comentaris

--- test/chrono.py
+++ test/chrono.py
@@ -29,7 +29,9 @@ class Chrono(QtWidgets.QMainWindow):
         self.showLCD() 
 
     def record_lap(self):
-        
+        """
+        Afegeix una nova lap al cronómetro
+        """
         this_time = datetime.timedelta(milliseconds=self.mscounter)
         if (self.mscounter != 0):
             self.lap_num += 1
@@ -46,6 +48,9 @@ class Chrono(QtWidgets.QMainWindow):
 
 
     def showLCD(self):
+        """
+        Aquesta funció serveix per a mostrar per pantalla el temps
+        """
         text = str(datetime.timedelta(milliseconds=self.mscounter))[:-3]
         self.cronNum.setDigitCount(8)
         if not self.isreset:  # si "isreset" es False
@@ -54,10 +59,16 @@ class Chrono(QtWidgets.QMainWindow):
             self.cronNum.display('0:00.000')
 
     def run_watch(self):
+        """
+        Executa el cronómetre de forma interna
+        """
         self.mscounter += 1
         self.showLCD()
 
     def start_crono(self):
+        """
+        Comença el cronómetre de forma visible
+        """
         self.timer.start()
         
 
@@ -72,6 +83,10 @@ class Chrono(QtWidgets.QMainWindow):
         self.isreset = False
         
     def pause_watch(self):
+        """
+        Pausa el cronómetre
+        """
+
         self.timer.stop()
 
         self.startStop.setIcon(self.start_image)
@@ -83,6 +98,9 @@ class Chrono(QtWidgets.QMainWindow):
         self.lap.clicked.connect(self.reset_watch)
     
     def reset_watch(self):
+        """
+        Reinicia el cronómetre
+        """
         self.timer.stop()
         self.mscounter = 0
         self.lap_num = 0
@@ -99,7 +117,7 @@ class Chrono(QtWidgets.QMainWindow):
         self.lap.clicked.connect(self.record_lap)
 
 
-        for i in reversed(range(self.lapsLayout.count())): 
+        for i in reversed(range(self.lapsLayout.count())): # Esborra totes les laps
             self.lapsLayout.itemAt(i).widget().setParent(None)
         
 
