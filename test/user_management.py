468626a9545f130f3c6d79fa2a354967528adb11
Àlex <alexcrafteo1@outlook.es>
2020-11-18 11:27:09 +0100

    Ara la llista mostra tots els usuaris

--- test/user_management.py
+++ test/user_management.py
@@ -1,12 +1,33 @@
 from PyQt5 import QtWidgets, uic, QtCore, QtGui
 import sys
-from login import getOsSeparator
+import login
+import sqlite_connector as sqlite
+import chrono
 
 class Users_management(QtWidgets.QMainWindow):
     def __init__(self):
         super(Users_management, self).__init__() # Call the inherited classes __init__ method
-        uic.loadUi('UI'+getOsSeparator()+'users.ui', self) # Load the .ui file
+        uic.loadUi('UI'+login.getOsSeparator()+'users.ui', self) # Load the .ui file
         self.show() # Show the GUI
+
+        self.cronIcon.clicked.connect(self.return_to_chrono)
+        self.show_users()
+
+    def show_users(self):
+        model = QtGui.QStandardItemModel()
+        self.usersList.setModel(model)
+        sql_con = sqlite.sqlite_connector(login.getDB())
+        users = sql_con.get_users()
+        for user in users:
+            row = QtGui.QStandardItem(user[0])
+            model.appendRow(row)
+
+        
+
+    def return_to_chrono(self):
+        self.new_window = chrono.Chrono()
+        self.new_window.show()
+        self.close()
         
 # Eliminar aço despres de acabar les proves ja que no volem que es puga executar
 if __name__ == "__main__":
