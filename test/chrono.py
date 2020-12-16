7d71c748d053b03c630d7590c92dbfcb5a91c46d
Àlex <alexcrafteo1@outlook.es>
2020-12-16 13:18:34 +0100

    Bugfix

--- test/chrono.py
+++ test/chrono.py
@@ -125,8 +125,8 @@ class Chrono(QtWidgets.QMainWindow):
 
     def get_lap_type(self, lap, time):
         if(lap == -1): # -1 és temps total
-            minTotalTime = self.config.get('TotalTimeSection', 'minimumTime')
-            maxTotalTime = self.config.get('TotalTimeSection', 'maxiumumTime')
+            minTotalTime = float(self.config.get('TotalTimeSection', 'minimumTime'))
+            maxTotalTime = float(self.config.get('TotalTimeSection', 'maxiumumTime'))
             if(time < minTotalTime):
                 return "Leve"
             elif(time > maxTotalTime):
@@ -134,8 +134,8 @@ class Chrono(QtWidgets.QMainWindow):
             else:
                 return "Moderado"
         elif(lap == 0):
-            minTime = self.config.get('Seg1TimeSection', 'minimumTime')
-            maxTime = self.config.get('Seg1TimeSection', 'maxiumumTime')
+            minTime = float(self.config.get('Seg1TimeSection', 'minimumTime'))
+            maxTime = float(self.config.get('Seg1TimeSection', 'maxiumumTime'))
             if(time < minTime):
                 return "Leve"
             elif(time > maxTime):
@@ -143,8 +143,8 @@ class Chrono(QtWidgets.QMainWindow):
             else:
                 return "Moderado"
         elif(lap == 1):
-            minTime = self.config.get('Seg2TimeSection', 'minimumTime')
-            maxTime = self.config.get('Seg2TimeSection', 'maxiumumTime')
+            minTime = float(self.config.get('Seg2TimeSection', 'minimumTime'))
+            maxTime = float(self.config.get('Seg2TimeSection', 'maxiumumTime'))
             if(time < minTime):
                 return "Leve"
             elif(time > maxTime):
@@ -152,8 +152,8 @@ class Chrono(QtWidgets.QMainWindow):
             else:
                 return "Moderado"
         else:
-            minTime = self.config.get('Seg3TimeSection', 'minimumTime')
-            maxTime = self.config.get('Seg3TimeSection', 'maxiumumTime')
+            minTime = float(self.config.get('Seg3TimeSection', 'minimumTime'))
+            maxTime = float(self.config.get('Seg3TimeSection', 'maxiumumTime'))
             if(time < minTime):
                 return "Leve"
             elif(time > maxTime):
