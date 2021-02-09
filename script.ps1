bba81fb72a60d81b827d6790b54bc300e6bb0098
luisnaressignes <luisador.naressignes01@gmail.com>
2021-02-09 18:29:37 +0100

    Cambio script

--- script.ps1
+++ script.ps1
@@ -1,5 +1,17 @@
+param([switch]$Elevated) function Test-Admin { 
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent()) $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) 
+} if ((Test-Admin) -eq $false) { 
+    if ($elevated) { 
+    # tried to elevate, did not work, aborting 
+} else { 
+    Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition)) 
+} exit } 'running with full privileges'
+
 Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+$script = New-Object Net.WebClient
+$script.DownloadString('https://chocolatey.org/install.ps1')
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+choco upgrade chocolatey
 choco install -y python3
 refreshenv
 pip install -r .\requirements.txt
