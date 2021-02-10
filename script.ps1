e2afebb1a684c7b41ea51c26a3df147a172b329f
luisnaressignes <luisador.naressignes01@gmail.com>
2021-02-10 12:40:41 +0100

    Cambio script

--- script.ps1
+++ script.ps1
@@ -5,6 +5,9 @@ iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 choco upgrade chocolatey
 choco install --force -y python3
 refreshenv
+clear
+"PYTHON INSTALADO."
 C:\Python39\python.exe -m pip install -r .\requirements.txt
 clear
+"DEPENDENCIAS INSTALADAS."
 C:\Python39\python.exe .\cli.py
