b22b5ff1facf2bf1e1239bd53c272c06312931a0
Àlex <alexcrafteo1@outlook.es>
2021-02-11 17:43:45 +0100

    Ensurepip bugfix

--- script.ps1
+++ script.ps1
@@ -4,13 +4,9 @@ $script.DownloadString('https://chocolatey.org/install.ps1')
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 choco upgrade chocolatey
 choco install --force -y python3
-refreshenv
-<<<<<<< HEAD
 C:\Python39\python.exe -m ensurepip
-=======
 clear
 "PYTHON INSTALADO."
->>>>>>> 89b86f007e2a3b3a003c320b38939495580807a9
 C:\Python39\python.exe -m pip install -r .\requirements.txt
 clear
 "DEPENDENCIAS INSTALADAS."
